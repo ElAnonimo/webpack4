ed084d9ff904dbeb053fa734a6838f1ac2da9395
elanonimo <mgrbmk@emailhosting.com>
2018-07-01 14:18:38 +0300

    added markdown data

--- src/components/AppRoot.js
+++ src/components/AppRoot.js
@@ -11,8 +11,8 @@ export default class Counter extends React.Component {
 		return (
 			<div className="profile">
 				<img src={require('../images/400.jpg')} alt="" />
-				{/* <h1>{this.props.data.heading}</h1> */}
-				{/* <div className="content">{this.props.data.bio}</div> */}
+				<h1>{this.props.data.heading}</h1>
+				<div className="content">{this.props.data.bio}</div>
 				<div className="content">{markdownData.title}</div>
 				<div className="content">{markdownData.author}</div>
 				<div className="content" dangerouslySetInnerHTML={{ __html: markdownData.__content }} />
